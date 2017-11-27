cb36507c67a52b4557d8bc206c8ff05d07be5325
9AD589B1 <kocha017@gmail.com>
2017-11-27 12:53:10 -0600

    Initial commit

---
+++ SampleProportionConfidenceInterval.R
@@ -0,0 +1,13 @@
+#The general social survey (GSS) found that 571/670 (85%) people answered the 
+#question on experiment design correctly. Using a 95% confidence interval, we want
+#to estimate the proportion of all Americans who have good intuition about experiment design. 
+
+rm ( list = ls ( all = TRUE ) ) #clear variables
+
+s <- 670 #sample size
+phat <- 571 / 670 #calculate the sample proportion
+nonphat <- 1 - phat
+z <- 1.96 #Z-Score for the the 95% confidence intervale
+se <- sqrt ((phat * nonphat) / s)
+CI <- c(phat - se, phat + se)
+
---
+++ SampleProportionProbability.R
@@ -0,0 +1,20 @@
+#90% of all plant species are classified as angiosperms. These are flowering plants. 
+#If you were to randomly sample 200 plants from the list of all known plant species, 
+#what is the probability that at least 95% of the plants in your sample will be flowering plants?
+
+rm ( list = ls ( all = TRUE ) ) #clear variables
+
+s <- 200 #sample size
+phat <- 0.95 #probability of our sample containing 95% angiosperms
+p <- 0.9 #population proportion of angiosperms
+nonp <- 1 - p #population proportion of non-angiosperms
+x <- 0.9 #population mean
+se <- sqrt ((p * nonp) / s) #Calculate the Standard Error
+z <- ( phat - p ) / se #Calculate Z-Score
+prob <- pnorm(z, lower.tail = FALSE) #calculate aread under the curve to obtain the probability
+prob
+
+#Alternatively, we can use a binomial distribution to calculate the probability
+
+binomprob <- sum(dbinom(190:200, 200, 0.9))
+binomprob
