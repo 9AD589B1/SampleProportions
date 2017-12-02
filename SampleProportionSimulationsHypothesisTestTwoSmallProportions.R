cbae36b721ef2caf548e9d5e11f6ee25c5dc37af
9AD589B1 <kocha017@gmail.com>
2017-12-02 17:57:20 -0600

    Changes

--- SampleProportionSimulationsHypothesisTestTwoSmallProportions.R
+++ SampleProportionSimulationsHypothesisTestTwoSmallProportions.R
@@ -16,25 +16,66 @@
 
 rm (list = ls (all = TRUE))
 
+suppressWarnings ( suppressMessages ( library ( dplyr ) ) )
+
 set.seed (123)
+
+ObservedDiff <- (11/12) - (7/12)
 
 CorrectGuesses <- c(rep.int(1, 18))
 IncorrectGuesses <- c(rep.int(0, 6))
 CombinedGuesses <- c(CorrectGuesses, IncorrectGuesses)
 
 nrows <- 1000000
-Guesses <- matrix(NA, nrow = nrows, ncol = 24)
+GuessTrials <- matrix(NA, nrow = nrows, ncol = 24)
 
 for (n in 1:nrows)
-  Guesses[n, ] <-
+  GuessTrials[n, ] <-
   c(unlist(split(
     sample(CombinedGuesses), sample(rep(1:2, c(12, 12)), replace = FALSE)
   ), use.names = FALSE))
 
-Guesses <- as.data.frame (Guesses)
+GuessTrials <- as.data.frame (GuessTrials)
 
-GuessesSuccess <-
+GuessesSuccessPercent <-
   data.frame (
-    BackSuccess = apply (Guesses[1:12], 1, mean, na.rm=TRUE),
-    PalmSuccess = apply (Guesses[13:24], 1, mean, na.rm=TRUE)
+    BackSuccess = apply (GuessTrials[1:12], 1, mean, na.rm=TRUE),
+    PalmSuccess = apply (GuessTrials[13:24], 1, mean, na.rm=TRUE),
+    SuccessDiff = apply (GuessTrials[1:12], 1, mean, na.rm=TRUE) - apply (GuessTrials[13:24], 1, mean, na.rm=TRUE)
   )
+
+GuessesSuccessTotal <-
+  data.frame (
+    BackSuccessTotal = apply (GuessTrials[1:12], 1, function(x)
+      sum(x == 1)),
+    PalmSuccessTotal = apply (GuessTrials[13:24], 1, function(x)
+      sum(x == 1))
+  )
+
+GuessSuccessTotalPercentvsObserved <- GuessesSuccessPercent %>%
+  summarise (
+    TotalSuccessCount   = sum(SuccessDiff >= ObservedDiff),
+    TotalSuccessPercent = sum(SuccessDiff >= ObservedDiff) / nrows
+  )
+
+GuessSuccessTotalPercent <- GuessesSuccessTotal %>%
+  summarise (
+    TotalBackSuccessCount   = sum(BackSuccessTotal),
+    TotalPalmSuccessCount   = sum(PalmSuccessTotal),
+    TotalBackSuccessPercent = sum(BackSuccessTotal) / (12 * nrows),
+    TotalPalmSuccessPercent = sum(PalmSuccessTotal) / (12 * nrows),
+    TotalSuccessPercentDiff = (sum(BackSuccessTotal) / (12 * nrows)) - (sum(PalmSuccessTotal) / (12 * nrows))
+  )
+
+
+backtrials <- (12 * nrows)
+palmtrials <- (12 * nrows)
+phatback <- GuessSuccessTotalPercent$TotalBackSuccessPercent
+phatpalm <- GuessSuccessTotalPercent$TotalPalmSuccessPercent
+phatpool <- (GuessSuccessTotalPercent$TotalBackSuccessCount + GuessSuccessTotalPercent$TotalPalmSuccessCount) / (24 * nrows)
+nonphatpool <- 1 - phatpool
+nullvalue <- 0
+pointestimate <- abs ( phatback - phatpalm )
+se <- sqrt ( ( ( phatpool * nonphatpool ) / backtrials ) + ( ( phatpool * nonphatpool ) / palmtrials ) )
+z <- ( pointestimate - nullvalue ) / se
+pvalue <- pnorm ( abs(z), lower.tail = FALSE ) * 2
